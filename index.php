d7cfeedac2a73f1edae9f758c2668193efd30f27
aliemad6969 <88372525+aliemad6969@users.noreply.github.com>
2021-08-03 10:27:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*   
+Coded by Kerolos Atef Saber (MR_WISE)                        
+*/
+
+session_start();
+error_reporting(0);
+
+include "./bots1.php";
+include "./bots2.php";
+include "./bots3.php";
+include "./bots4.php";
+
+
+?>
